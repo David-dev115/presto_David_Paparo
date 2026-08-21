4150754bac8a1d18fec6177c78d0ac84eedde707
David Paparo <d.paparotiscali.it>
2026-08-21 17:58:37 +0200

    migliorie grafiche_1

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,8 +1,8 @@
 
-<nav id="navbar" class="navbar navbar-expand-lg fixed-top bg-grey">
+<nav id="navbar" class="navbar navbar-expand-lg fixed-top">
     <div class="container-fluid">
         <a class="navbar-brand nav-link" href="#">
-            <img src="/media/logo-black.png" alt="logo" class="logo">
+            <img src="/media/logo-blue.png" alt="logo" class="logo">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
