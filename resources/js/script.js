221fa1d12c802339f2d8ebecd9d3333ee1086f4e
David Paparo <d.paparo@tiscali.it>
2026-08-22 06:12:26 +0200

    migliorie grafiche_2

--- resources/js/script.js
+++ resources/js/script.js
@@ -3,19 +3,23 @@
 
 let navbar = document.querySelector('#navbar');
 
-window.addEventListener('scroll' , ()=> {
+// window.addEventListener('scroll' , ()=> {
 
-    let scrolled = window.scrollY ;
+//     let scrolled = window.scrollY ;
 
-    if (scrolled > 0){
-        navbar.style.height='80px';
-    } else {
-        navbar.style.height='60px';
+//     if (scrolled > 0){
+//         navbar.style.height='80px';
+//     } else {
+//         navbar.style.height='72px';
 
-    }
+//     }
 
 
-})
+// })
+
+window.addEventListener('scroll', () => {
+    navbar.classList.toggle('scrolled', window.scrollY > 10);
+});
 
 
 
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 
 <nav id="navbar" class="navbar navbar-expand-lg fixed-top">
     <div class="container-fluid">
-        <a class="navbar-brand nav-link" href="#">
+        <a class="navbar-brand " href="#">
             <img src="/media/logo-blue.png" alt="logo" class="logo">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
