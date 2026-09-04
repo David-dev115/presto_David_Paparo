92c3fc08e4979df4345dbff1a69834d5b3032d54
David Paparo <d.paparo@tiscali.it>
2026-09-04 03:16:52 +0200

    User Story 8 completata

--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -17,6 +17,8 @@ use App\Jobs\ResizeImage;
 use App\Jobs\GoogleVisionSafeSearch;
 use App\Jobs\GoogleVisionLabelImage;
 
+use App\Jobs\RemoveFaces;
+
 class CreateArticleForm extends Component
 {   
     use WithFileUploads;
@@ -62,9 +64,14 @@ class CreateArticleForm extends Component
             $newFileName = "articles/{$this->article->id}";
             $newImage = $this->article->images()->create(['path'=> $image->store($newFileName, 'public')]);
 
-            dispatch(new ResizeImage($newImage->path, 300, 300));
-            dispatch(new GoogleVisionSafeSearch($newImage->id));
-            dispatch(new GoogleVisionLabelImage($newImage->id));
+            // dispatch(new ResizeImage($newImage->path, 300, 300));
+            // dispatch(new GoogleVisionSafeSearch($newImage->id));
+            // dispatch(new GoogleVisionLabelImage($newImage->id));
+        RemoveFaces::withChain([
+	       new ResizeImage($newImage->path, 300, 300),
+           new GoogleVisionSafeSearch($newImage->id),
+           new GoogleVisionLabelImage($newImage->id)
+		   ])->dispatch($newImage->id);
         }
 
         File::deleteDirectory(storage_path('/app/livewire-tmp'));
